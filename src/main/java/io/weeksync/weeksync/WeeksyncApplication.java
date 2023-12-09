96a67785f14a02912e458c61cc10828c2d9b9a9a
Nilliam Cris Ometto <nilliamometto@gmail.com>
2023-12-09 18:17:26 -0300

    initial

---
+++ src/main/java/io/weeksync/weeksync/WeeksyncApplication.java
@@ -0,0 +1,13 @@
+package io.weeksync.weeksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeksyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeksyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/weeksync/weeksync/WeeksyncApplicationTests.java
@@ -0,0 +1,13 @@
+package io.weeksync.weeksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeksyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
