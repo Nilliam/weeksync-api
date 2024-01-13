68a7312a7c2ea087e87050b9b91810c03d76ad2d
Nilliam Cris Ometto <nilliamometto@gmail.com>
2024-01-13 17:49:19 -0300

    fix: adding parent_id column to task relation

--- src/main/resources/db/migration/V2__Task.sql
+++ src/main/resources/db/migration/V2__Task.sql
@@ -5,5 +5,7 @@ CREATE TABLE task (
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
     status VARCHAR,
     account_id UUID REFERENCES account(id),
-    CONSTRAINT task_account_id_foreign_key FOREIGN KEY (account_id) REFERENCES account(id)
+    parent_id UUID REFERENCES task(id),
+    CONSTRAINT task_account_id_foreign_key FOREIGN KEY (account_id) REFERENCES account(id),
+    CONSTRAINT task_parent_id_foreign_key FOREIGN KEY (parent_id) REFERENCES task(id)
 );
